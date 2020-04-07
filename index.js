f92c94c8495c3552ed84c29dee4bb5566138d01d
Ogtsn99 <fantasista4123@icloud.com>
2020-04-07 14:22:17 +0900

    add isMultipleOfSeventeen

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+/**
+ * multiple of 17: TRUE else: FALSE
+ * @param {number} num 
+ */
+function isMultipleOfSeventeen(num) {
+    return !(num % 17);
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
